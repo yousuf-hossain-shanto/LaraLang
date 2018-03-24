ad6879a94db51d06e04fced10a9ba444fb51817a
yousuf-hossain-shanto <yousuf.hossain.shanto@gmail.com>
2018-03-24 13:59:24 +0600

    Initial Commit

---
+++ src/LangServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace YHShanto\LaraLang;
+
+use Illuminate\Support\ServiceProvider;
+
+class LangServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
